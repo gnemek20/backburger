904eab35f5120a5d3d88e09ae23b1cb10e1b2dad
gnemek20 <gnemek20@gmail.com>
2024-04-30 07:58:13 +0900

    postRequest 대부분 주석 처리

--- api/index.ts
+++ api/index.ts
@@ -31,30 +31,30 @@ app.get('/', (req, res) => {
 app.post('/postRequest', async (req, res) => {
   const { name, contact, detail, files } = JSON.parse(req.body);
 
-  interface fileDictionaryProps {
-    filename: string
-    path: string
-  }
+  // interface fileDictionaryProps {
+  //   filename: string
+  //   path: string
+  // }
 
-  let filesDictionary: Array<fileDictionaryProps> = [];
-  files && files.map((file) => {
-    filesDictionary.push({
-      filename: file.name,
-      path: file.path
-    });
-  });
+  // let filesDictionary: Array<fileDictionaryProps> = [];
+  // files && files.map((file) => {
+  //   filesDictionary.push({
+  //     filename: file.name,
+  //     path: file.path
+  //   });
+  // });
 
-  const mailOptions = {
-    from: 'yeou914@gmail.com',
-    to: 'yeou914@gmail.com',
-    subject: `${name}님의 요청사항입니다.`,
-    html: `
-      <p>연락처: ${contact}</p>
-      <br />
-      <p>${detail && detail.replace(/\n/g, '<br />')}</p>
-    `,
-    attachments: filesDictionary
-  }
+  // const mailOptions = {
+  //   from: 'yeou914@gmail.com',
+  //   to: 'yeou914@gmail.com',
+  //   subject: `${name}님의 요청사항입니다.`,
+  //   html: `
+  //     <p>연락처: ${contact}</p>
+  //     <br />
+  //     <p>${detail && detail.replace(/\n/g, '<br />')}</p>
+  //   `,
+  //   attachments: filesDictionary
+  // }
 
   try {
     // await transporter.sendMail(mailOptions);
